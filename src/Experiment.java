b9b9043d94e2682144b4c90e165e88bdc07c531f
Alexg11111 <141282391+Alexg11111@users.noreply.github.com>
2026-04-06 17:28:04 -0600

    Update Experiment.java

--- src/Experiment.java
+++ src/Experiment.java
@@ -21,4 +21,22 @@ public class Experiment {
         return String.format("#%-2d %-30s  Weight: %3d kg  Rating: %d",
                 number, name, weight, rating);
     }
+	//overiding for junit testing
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) return true;
+        if (obj == null || getClass() != obj.getClass()) return false;
+        
+        Experiment other = (Experiment) obj;
+        
+        return number == other.number &&
+               weight == other.weight &&
+               rating == other.rating &&
+               Objects.equals(name, other.name);
+    }
+    
+    @Override
+    public int hashCode() {
+        return Objects.hash(number, name, weight, rating);
+    }
 }
